5e3a5e1e081fcf51ff66fcbebfb22f3c6108d308
FrancescaMassaioli <73820900+FrancescaMassaioli@users.noreply.github.com>
2021-10-15 10:59:43 +0200

    Create R_first_code.r

---
+++ R_first_code.r
@@ -0,0 +1,8 @@
+# This is my first code in github! Quite exciting, right? 
+
+# Here are the input
+
+water <- c(100, 200, 300, 400, 500)
+
+# Marta data on fighes genomes
+fishes <- c(10, 50, 60, 100, 200)
