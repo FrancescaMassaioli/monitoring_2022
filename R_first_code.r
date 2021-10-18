0bc9ba5ee724f48fb91d6e59dc6a4bb43e229137
FrancescaMassaioli <73820900+FrancescaMassaioli@users.noreply.github.com>
2021-10-18 10:57:52 +0200

    Update R_first_code.r

--- R_first_code.r
+++ R_first_code.r
@@ -6,3 +6,10 @@ water <- c(100, 200, 300, 400, 500)
 
 # Marta data on fighes genomes
 fishes <- c(10, 50, 60, 100, 200)
+
+
+
+# the data we developed can be stored in a table
+# a table in R is called data frame 
+
+streams <- data.frame(water, fishes) 
