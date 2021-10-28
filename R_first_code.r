af673a37f333c79d0d935b4b40988b5e77512990
FrancescaMassaioli <73820900+FrancescaMassaioli@users.noreply.github.com>
2021-10-28 11:09:15 +0200

    Update R_first_code.r

--- R_first_code.r
+++ R_first_code.r
@@ -14,6 +14,36 @@ fishes <- c(10, 50, 60, 100, 200)
 
 streams <- data.frame(water, fishes) 
 
+
 streams
+ 
 
 View(streams)
+
+plot(streams) 
+
+#now we are going import and export data!
+
+write.table(streams, file="my_first_table.txt") 
+
+# some collegues did send us a table How to put it into R? 
+
+read.table(file="my_first_table.txt")
+
+# let's assign it to an object inside R
+
+francescatable <- read.table(file="my_first_table.txt")
+
+
+# the first statistic for lazy beautiful people 
+
+summary(ducciotable)
+
+# infos only on fishes 
+
+summary(francescatable$fishes)
+
+# Histogram
+
+hist(francescatable$fishes)
+
