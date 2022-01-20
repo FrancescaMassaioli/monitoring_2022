c8feb0badfca9716b9b235300ce21aebcab9be6d
FrancescaMassaioli <73820900+FrancescaMassaioli@users.noreply.github.com>
2022-01-20 22:34:36 +0100

    Update R_code_exame.r

--- R_code_exame.r
+++ R_code_exame.r
@@ -83,11 +83,6 @@ p1 / p2
 
 
 #day 2 
-
-
-r2014 <- raster("c_gls_NDVI300_201405010000_GLOBE_PROBAV_V1.0.1.nc")
-
-b2014 <- brick(r2014)
 # you can crop your image on a certain area: you can put the extention you want to crop
 
 # longitude (lambda) from 0 to 20
@@ -95,8 +90,112 @@ b2014 <- brick(r2014)
 
 # crop the stack to the extent of Madagascar
 
-ext <- extent(36, 56, -32, -3)
+r2014 <- raster("c_gls_NDVI300_201405010000_GLOBE_PROBAV_V1.0.1.nc")
 
-crop2014 <- crop(b2014, ext)
+b2014 <- brick("c_gls_NDVI300_201405010000_GLOBE_PROBAV_V1.0.1.nc")
+ext <- extent(36, 56, -32, -3)
+c2014 <- crop(b2014, ext)
+plot(c3014, col=cl)
+
+r2020 <- raster("c_gls_NDVI300_202005210000_GLOBE_PROBAV_V1.0.1.nc")
+b2020 <- brick("c_gls_NDVI300_202005210000_GLOBE_PROBAV_V1.0.1.nc")
+ext <- extent(36, 56, -32, -3)
+c2020 <- crop(b2014, ext)
+plot(c2020, col=cl)
+
+#let's put the images one beside the other with par function
+
+par(mfrow=c(1,2))
+plot(c3014, col=cl)
+plot(c2020, col=cl)
+
+
+#ggplot function
+ggplot() +
+geom_raster(c2014, mapping = aes(x=x, y=y, fill=layer)) +
+scale_fill_viridis(option="cividis")
+ggtitle("Forest 2014")
+
+ggplot() +
+geom_raster(c2014, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.333M)) +
+scale_fill_viridis(option="magma")
+ggtitle("Forest 2020")
+
+# to select files from my lab folder in a quick way: list.files
+#importing all the data together with the lapply function
+
+rlist <- list.files(pattern="NDVI300_20")
+
+# we import the data for single layer, but at the same time togheter
+
+list_rast <- lapply(rlist, raster)
+list_rast
+
+forestack <- stack(list_rast)
+forestac
+######################################################################
+
+library(rgdal)
+
+ # importing the data ENERGY
+
+
+r1999 <- raster("c_gls_ALDH_199905130000_GLOBE_VGT_V1.4.1.nc")
+
+b1999 <- brick("c_gls_ALDH_199905130000_GLOBE_VGT_V1.4.1.nc")
+
+ext <- extent(41, 52, -28, -10)
+c1999 <- crop(b1999, ext)
+plot
+
+
+
+r2020 <- raster("c_gls_ALDH_202003240000_GLOBE_PROBAV_V1.5.1.nc")
+
+b2020 <- brick("c_gls_ALDH_202003240000_GLOBE_PROBAV_V1.5.1.nc")
+
+ext <- extent(41, 52, -28, -10)
+c2020 <- crop(b2020, ext)
+plot
+
+
+#import image
+
+
+list <- list.files(pattern="albedo")
+list
+# [1] "albedo1999_.jpeg" "albedo2020_.jpeg"
+a1999 <- brick("albedo1999_.jpeg")
+
+a2020 <- brick("albedo2020_.jpeg")
+
+#plotRGB
+plotRGB(a1999, r=2, g=3, b=1, stretch="Lin")
+
+plotRGB(a2020, r=2, g=3, b=1, stretch="Lin")
+
+# let's calculate energy in 1999
+dvi1999 <- a1999$albedo1999_.1 - a1999$albedo1999_.2
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
+plot(dvi1999, col=cl)
+
+dvi2020 <- a2020$albedo2020_.1 - a2020$albedo2020_.2
+plot(dvi2020, col=cl)
+
+# differencing two images of energy in two different times
+
+dvidif <- dvi1999 - dvi2020
+cld <- colorRampPalette(c('blue','white','red'))(100)
+
+
+par(mfrow=c(3,2))
+plotRGB(a1999, r=2, g=3, b=1, stretch="Lin")
+plotRGB(a2020, r=2, g=3, b=1, stretch="Lin")
+plot(dvi1999, col=cl)
+plot(dvi2020, col=cl)
+plot(dvidif, col=cld)
+
+
+
 
 
