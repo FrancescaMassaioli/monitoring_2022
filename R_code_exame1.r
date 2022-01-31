fdea0e894e0dbca2fcecef763ebdc2e462b46308
FrancescaMassaioli <73820900+FrancescaMassaioli@users.noreply.github.com>
2022-01-31 16:20:10 +0100

    Update R_code_exame1.r

--- R_code_exame1.r
+++ R_code_exame1.r
@@ -1,12 +1,11 @@
-# I want to analyse changes in vegetation state in Madagascar
+# multi-temporal analysis in vegetation state in Madagascar 
 
-library(ncdf4) 
+library(ncdf4) # for uploading and visualizing copernicus data in R
 library(raster)  #to manage satellite data 
 library(RStoolbox) #classification
-library(viridis)
+library(viridis) #brings to R color scales designed to improve graph readability
 library(ggplot2) # plotting with aestethics
-library(patchwork)
-library(rgdal)
+library(patchwork) #to combine multiple ggplot2 plots in a plot composition
 library(gridExtra) #for grid.arrange plotting, creating a multiframe 
 
 
@@ -14,12 +13,12 @@ library(gridExtra) #for grid.arrange plotting, creating a multiframe
 
 setwd("C:/Users/franc/Desktop/lab/exam")
 
-# create the list of files stored in exam folder starting from a pattern using list.files function
+# create the list of files stored in exam folder starting from a pattern "TOCR" using list.files function
 
 import <- list.files(pattern="TOCR")
 import 
 
-# create a raster file (using lapply function to apply raster function to the list)
+# create a raster file using lapply function to apply raster function to the list
 
 rast_list <- lapply(import, raster)
 rast_list
@@ -27,7 +26,7 @@ rast_list
 # renomanate both raster 
 r1999 <- rast_list[[1]]
 
-#crop the file to see only Madagascar 
+#crop both files to see only Madagascar 
 
 ext <- extent(41, 52, -28, -10)
 c1999 <- crop(r1999, ext)
@@ -56,8 +55,7 @@ scale_fill_viridis(option="C") +
 ggtitle("TOC 2018")
 
 
-#import image
-
+#importing the images from the previous plot 
 
 list <- list.files(pattern="TOC_")
 list
@@ -68,17 +66,17 @@ list
 
 list_T <- lapply(list, brick)
 
-#assign simple names
+#assign to the brick files new simple names
 
 TOC_1999 <- list_T[[1]]
 TOC_2018 <- list_T[[2]]
 
 
-#plotRGB 1999
+#first we make a plotRGB with the image of 1999 to divide the different layers in the red-green-blue channels 
 
 plotRGB(TOC_1999, r=1, g=2, b=3, stretch="Lin") 
 
-#for curiosity try also the red band in 
+#for curiosity I tried also the red band in 
 
 plotRGB(TOC_1999, r=3, g=2, b=1, stretch="Lin")
 
@@ -96,12 +94,13 @@ plotRGB(TOC_2018, r=1, g=2, b=3, stretch="Lin")
 
 
 
-# create a stack file to plot all layers at the same time 
+# create a stack file to plot all layers at the same time (I tried with different colours choices)
 
 cls <- colorRampPalette(c('darkblue', 'deepskyblue4', 'cyan3', 'pink', 'yellow', 'white'))(100)
 
 cls <- colorRampPalette(c('darkblue', 'deepskyblue4', 'cyan3', 'azure2', 'yellow', 'orange', 'chocolate1'))(100)
 
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
 
 s1999 <- stack(TOC_1999)
 plot(TOC_1999, col=cl, main="STACK 1999")
@@ -109,7 +108,7 @@ plot(TOC_1999, col=cl, main="STACK 1999")
 s2018 <- stack(TOC_2018)
 plot(TOC_2018, col=cl, main="STACK 2018")
 
-# calculate energy in 1999
+# I want to calculate energy in 1999, I make the difference between layer 1 (NIR) and layer 2 (red)
  
 dvi1999 <- s1999$TOC_1999.1 - s1999$TOC_1999.2 
 cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
@@ -126,16 +125,15 @@ dvidif <- dvi1999 - dvi2018
 cld <- colorRampPalette(c('blue','white','red'))(100) 
 plot(dvidif, col=cld, main="Difference in NDVI between 1999 and 2018")
 
-or NC <- colorRampPalette(c("chocolate4", "orange", "yellow", "grey", "green", "forestgreen", "darkgreen"))(100)
+# or NC <- colorRampPalette(c("chocolate4", "orange", "yellow", "grey", "green", "forestgreen", "darkgreen"))(100)
 
-# result: big difference of energy. 
-
+#par function to put all the plots togheter in a multiframe 
 par(mfrow=c(2,2))
 plot(dvi1999, col=cl, main="DVI 1999")
 plot(dvi2018, col=cl, main="DVI 2018")
 plot(dvidif, col=cld, main="Diff DVI 1999 and 2018")
 
-# see better how distributions is changing in a histogram
+#to see better how distributions is changing in a histogram
 
 hist(dvidif, col="limegreen", xlab="DVI change", main= "DVI histogram")
 
@@ -143,33 +141,40 @@ hist(dvidif, col="limegreen", xlab="DVI change", main= "DVI histogram")
 
 
 
-#LAI1km 
+#I want to deepen the analysis using LAI1km data. 
+#make a list of the data 
 
 rlist <- list.files(pattern="LAI")
 
+#choosing the extention of Madagascar 
 ext <- extent(41, 52, -28, -10)
 
+#importing the file creating a raster file
 lai_list  <- lapply(rlist, raster)
 
+#rename the file and cropping it
 l1999 <- lai_list[[2]]
 lai1999 <- crop(l1999, ext)
 lai1999
 
+#rename the file and cropping it
 l2019 <- lai_list[[1]]
 lai2019 <- crop(l2019, ext)
 lai2019
 
+#plotting them with a colour choice
 CLAI <- colorRampPalette(c("#1E377F","#0055CC","#0A75AD","#81D8D0","#FDD591","#FCA954","#ED7676"))(100)
 
 plot(lai1999, col=CLAI, main="LAI 1999")
 dev.off()
 plot(lai2019, col=CLAI, main="LAI 2019")
 
+#building a multiframe
 par(mfrow=c(1,2))
 plot(lai1999, col=CLAI, main="LAI 1999")
 plot(lai2019, col=CLAI, main="LAI 2019")
 
-#see the differences
+#see the differences between 1999 and 2019
 
 laidif <- lai2019 - lai1999
 plot(laidif, col=CLF2, main="Difference in LAI between 1999 and 2019")
@@ -177,7 +182,7 @@ CLF2 <- colorRampPalette(c("#B2182B","#D6604D","#F4A582","#FDDBC7","#D1E5F0","#9
 
 
 
-#ggplot
+#make a ggplot to use aestathics mapping and viridis scale
 
 p1 <- ggplot() + 
 geom_raster(lai1999, mapping = aes(x=x, y=y, fill=Leaf.Area.Index.1km )) +
@@ -195,35 +200,34 @@ ggtitle("LAI 2019")
 hist(lai1999, col="limegreen", xlab="LAI change", main= "LAI histogram")
 hist(lai2019, col="limegreen", xlab="LAI change", main= "LAI histogram")
 
+#build a multiframe 
+
 par(mfrow=c(1,2))
 hist(lai1999, col="limegreen", xlab="LAI change", main= "LAI histogram")
 hist(lai2019, col="limegreen", xlab="LAI change", main= "LAI histogram")
 
-#from hist to boxplot
-
-
+#from hist to boxplot, different way to see the distribution
 LAI <- stack(lai1999, lai2019)
 boxplot(LAI, vertical=T, axes=T, outline=F, col="limegreen", ylab="LAI", xlab="Year", main="LAI Boxplot")
-
-
-
-
 
 # plot one variable on top of the other. abline function: you can choose the intercept and the slope of the line
 
 plot(lai1999, lai2019, xlab='lai 1999', ylab='lai 2019')
 abline(0,1, col="red", x=lai1999, y=lai2019)
 
-#unsupervised classification --> to classify what are the forest and what baresoil area. we will use the Rtoolbox to make classification
-# unsuperClass(img, nClasses)
+#unsupervised classification --> to classify what are the different kind of landscape in Madagascar. we will use the Rtoolbox to make classification
 
+#make a list of the files
 lai_mad <- list.files(pattern="LAIT")
 
+#apply the brick function to the list 
 list_ <- lapply(lai_mad, brick)
-l1999b <- list_[[1]]
 
+#rename the files
+l1999b <- list_[[1]]
 l2019b <- list_[[2]]
 
+#unsuperClass function -> unsuperClass(img, nClasses)
 
 l1999c <- unsuperClass(l1999b, nClasses=3)
 l1999c
@@ -233,7 +237,7 @@ l2019c <- unsuperClass(l2019b, nClasses=3)
 l2019c
 plot(l2019c$map, col=CLAI, main="2019 classification")
 
-#frequencies 1999
+#calculate frequencies in 1999
 freq(l1999c$map)
 
 # value count
@@ -241,7 +245,7 @@ freq(l1999c$map)
 #[2,]  ocean    27306
 #[3,]  rainforest   3187
 
-#proportion:
+#calculate proportions from the frequencies:
 
 total <- 43766
 dryarea <- 13273/total
@@ -253,7 +257,7 @@ rainforest <- 3187/total
 #rainforest <- 0.07281908
 
 
-#build a dataframe
+#build a dataframe, first specify columns and then values 
 cover <- c("dry area", "ocean", "rainforest")
 prop1999 <- c(dryarea, ocean, rainforest)
 
@@ -270,7 +274,7 @@ proportion1999
 
 
 
-#freq 2019
+#calculate frequencies in 2019
 freq(l2019c$map)
 
  #  value count
@@ -305,16 +309,13 @@ proportion2019
 
 
 
-# final ggplot
-
-#ggplot
+# final ggplot using geom_bar layer in ggplot2 function
 
 ggplot(proportion1999, aes(x=cover, y=prop1999, color=cover)) + geom_bar(stat="identity", fill="white")
 
-
 ggplot(proportion2019, aes(x=cover, y=prop2019, color=cover)) + geom_bar(stat="identity", fill="white")
 
-#finale dataframe
+#final dataframe
 
 finalproportion <- data.frame(cover, prop1999, prop2019)
 
@@ -325,7 +326,7 @@ finalproportion <- data.frame(cover, prop1999, prop2019)
 
 
 
-# now we want to plot weverything all togheter -> gridExtra packages -> we are doing multiframe by using this package 
+# now I want to plot everything all togheter -> gridExtra packages -> we are doing multiframe by using this package 
 # 1 let's assign to every ggplot a name
 
 p1999 <- ggplot(proportion1999, aes(x=cover, y=prop1999, color=cover)) + geom_bar(stat="identity", fill="white")
