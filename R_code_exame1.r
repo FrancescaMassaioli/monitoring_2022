a01b664f56b4c3320226a13d7c98b3b4e4264acc
FrancescaMassaioli <73820900+FrancescaMassaioli@users.noreply.github.com>
2022-02-01 21:47:09 +0100

    Update R_code_exame1.r

--- R_code_exame1.r
+++ R_code_exame1.r
@@ -19,79 +19,60 @@ setwd("C:/Users/franc/Desktop/lab/exam")
 
 list.NDVI <- list.files(pattern="NDVI300")
 
+#using lapply function to apply the raster function over a list
+
 r.list <- lapply(list.NDVI, raster)
 r.list
 
+#rename the files with new simple names
 n.2014 <- r.list[[1]]
 n.2020 <- r.list[[2]]
 
+#cropping on Madagascar 
 
+ext <- extent(41, 52, -28, -10)
 c.2014 <- crop(n.2014, ext)
 c.2020 <- crop(n.2020, ext)
 
+# choosing a colorRampPalette and plotting
 NC <- colorRampPalette(c("chocolate4", "orange", "yellow", "grey", "green", "forestgreen", "darkgreen"))(100)
+
 plot(c.2014, col=NC, main="NDVI 2014")
 plot(c.2020, col=NC, main="NDVI 2020")
 
+#let's see the differences in NDVI between 2020 and 2014
 NDVIdif <- c.2020 - c.2014
-plot(NDVIdif, col=NC, main="Difference NDVI between 2014 and 2020")
+plot(NDVIdif, col=NC, main="Difference in NDVI between 2020 and 2014")
 
 
-#create a stack
-NDVIs <- stack(c.2014, c.2020)
-plot(NDVIs, col=cl)
+#############
 
-
-#boxplot of the stack
-boxplot(NDVIs, vertical=T, axes=T, outline=F, col="limegreen", ylab="NDVI", xlab="Year", main="NDVI")
-
-
-############################################
-
-# create the list of files stored in exam folder starting from a pattern "TOCR" using list.files function
+#create the list of files stored in exam folder starting from a pattern "TOCR" using list.files function
 
 import <- list.files(pattern="TOCR")
 import 
 
-# create a raster file using lapply function to apply raster function to the list
+#create a raster file using lapply function to apply raster function to the list
 
 rast_list <- lapply(import, raster)
 rast_list
 
 # renomanate both raster 
 r1999 <- rast_list[[1]]
+r2014 <- rast_list[[2]]
 
 #crop both files to see only Madagascar 
 
 ext <- extent(41, 52, -28, -10)
 c1999 <- crop(r1999, ext)
-
-r2014 <- rast_list[[2]]
 c2014 <- crop(r2014, ext)
 
-# plotting the images with a color choice
 
 cl <- colorRampPalette(c('darkblue', 'deepskyblue4', 'cyan3', 'pink', 'gold', 'white'))(100)
 cl <- colorRampPalette(c('darkblue', 'cyan3', 'pink', 'gold', 'white'))(100)
-plot(c1999, col=cl, main="TOC 1999")
-
-plot(c2014, col=cl, main="TOC 2014")
-
-  
-#ggplot function with viridis
-ggplot() + 
-geom_raster(b1999, mapping = aes(x=x, y=y, fill=Normalized.Top.Of.Canopy.Reflectance.in.Blue.band)) +
-scale_fill_viridis(option = "C") +
-ggtitle("TOC 1999")
-
-ggplot() +
-geom_raster(b2014, mapping = aes(x=x, y=y, fill=Normalized.Top.Of.Canopy.Reflectance.in.Blue.band)) +
-scale_fill_viridis(option="C") + 
-ggtitle("TOC 2014")
 
 
 #create a stack
-
 sTOC <- stack(c2014, c1999)
 plot(sTOC, col=NC)
 
@@ -103,18 +84,20 @@ boxplot(sTOC, vertical=T, axes=T, outline=F, col="limegreen", ylab="NDVI", xlab=
 
 #calculating Difference of TOC from stack differencing two images of energy in two different times
 
-L1 <- sTOC$Normalized.Top.Of.Canopy.Reflectance.in.Blue.band.1 - sTOC$Normalized.Top.Of.Canopy.Reflectance.in.Blue.band.2
-plot(L1, col=NC)
+diff <- sTOC$Normalized.Top.Of.Canopy.Reflectance.in.Blue.band.1 - sTOC$Normalized.Top.Of.Canopy.Reflectance.in.Blue.band.2
+CLF2 <- colorRampPalette(c("#B2182B","#D6604D","#F4A582","#FDDBC7","#D1E5F0","#92C5DE","#4393C3","#2166AC"))(100)
+
+plot(diff, col=CLF2, main="Difference in TOC between 2014 and 1999")
 
 #histogram
-hist(L1, col="limegreen", xlab="DVI", main="difference")
+hist(diff, col="limegreen", xlab="TOC distribution", main="TOC histogram")
 
-# fare par con hist e boxplot
-par(mfrow=c(2,2))
-hist(L1, col="limegreen", xlab="DVI", main="difference")
+#par with histogram and boxplot
+par(mfrow=c(1,2))
+hist(diff, col="limegreen", xlab="TOC distribution", main="TOC histogram"
 boxplot(sTOC, vertical=T, axes=T, outline=F, col="limegreen", ylab="NDVI", xlab="Year", main="TOC")
 
-
+############################################################
 
 #I want to deepen the analysis using LAI1km data. 
 #make a list of the data 
@@ -137,30 +120,6 @@ l2019 <- lai_list[[1]]
 lai2019 <- crop(l2019, ext)
 lai2019
 
-#plotting them with a colour choice
-CLAI <- colorRampPalette(c("#1E377F","#0055CC","#0A75AD","#81D8D0","#FDD591","#FCA954","#ED7676"))(100)
-
-plot(lai1999, col=CLAI, main="LAI 1999")
-dev.off()
-plot(lai2019, col=CLAI, main="LAI 2019")
-
-#building a multiframe
-par(mfrow=c(1,2))
-plot(lai1999, col=CLAI, main="LAI 1999")
-plot(lai2019, col=CLAI, main="LAI 2019")
-
-par(mfrow=c(1,2))
-plot(lai1999, col=NC, main="LAI 1999")
-plot(lai2019, col=NC, main="LAI 2019")
-
-#see the differences between 1999 and 2019
-
-laidif <- lai2019 - lai1999
-plot(laidif, col=CLF2, main="Difference in LAI between 1999 and 2019")
-CLF2 <- colorRampPalette(c("#B2182B","#D6604D","#F4A582","#FDDBC7","#D1E5F0","#92C5DE","#4393C3","#2166AC"))(100)
-
-
-
 #make a ggplot to use aestathics mapping and viridis scale
 
 p1 <- ggplot() + 
@@ -173,17 +132,21 @@ geom_raster(lai2019, mapping = aes(x=x, y=y, fill=Leaf.Area.Index.1km )) +
 scale_fill_viridis(option="plasma") +
 ggtitle("LAI 2019")
 
+#using grid.arrage function to build a multiframe
+grid.arrange(p1, p2, nrow=1)
+
+
 
 #hist function to see more in detail trough a division of LAI in classes 
 
-hist(lai1999, col="limegreen", xlab="LAI change", main= "LAI histogram")
-hist(lai2019, col="limegreen", xlab="LAI change", main= "LAI histogram")
+hist(lai1999, col="limegreen", xlab="LAI change", main= "LAI 1999")
+hist(lai2019, col="limegreen", xlab="LAI change", main= "LAI 2019")
 
 #build a multiframe 
 
 par(mfrow=c(1,2))
-hist(lai1999, col="limegreen", xlab="LAI change", main= "LAI histogram")
-hist(lai2019, col="limegreen", xlab="LAI change", main= "LAI histogram")
+hist(lai1999, col="limegreen", xlab="LAI", main= "LAI 1999")
+hist(lai2019, col="limegreen", xlab="LAI", main= "LAI 2019")
 
 #from hist to boxplot, different way to see the distribution
 LAI <- stack(lai1999, lai2019)
